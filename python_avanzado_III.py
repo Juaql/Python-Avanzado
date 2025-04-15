cb7c27d7e8ad8b8e1c188fd5940e61aeae464452
Juaql <148398822+Juaql@users.noreply.github.com>
2025-04-15 13:27:44 -0300

    Add files via upload

---
+++ python_avanzado_III.py
@@ -0,0 +1,11 @@
+"""
+Este archivo reproduce los codigos mostrados como ejemplo en el
+PDF Python Avanzado: OOP
+"""
+
+import os
+
+os.getcwd() # 'C:\\Python34'
+
+os.chdir('C:\\Windows\\System32')
+os.getcwd() # 'C:\\Windows\\System32'
