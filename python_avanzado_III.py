e69be86cebd3fd92c3cfe467ba8a8d9dfce82a02
Juaql <148398822+Juaql@users.noreply.github.com>
2025-04-16 09:20:01 -0300

    python_avanzado_III.py

--- python_avanzado_III.py
+++ python_avanzado_III.py
@@ -9,3 +9,52 @@ os.getcwd() # 'C:\\Python34'
 
 os.chdir('C:\\Windows\\System32')
 os.getcwd() # 'C:\\Windows\\System32'
+
+ruta = 'C:/Windows/System32'
+print(os.path.exists(ruta)) # True si existe, False si no
+
+archivo = 'C:/Windows/System32/notepad.exe'
+print(os.path.isfile(archivo)) # True si es archivo, False si no
+
+carpeta = 'C:/Windows/System32'
+print(os.path.isdir(carpeta)) # True si es carpeta, False si no
+
+ruta_completa = 'C:/Users/Usuario/Documentos/proyecto/final.py'
+
+directorio, archivo = os.path.split(ruta_completa)
+
+print("Directorio:", directorio)
+print("Archivo:", archivo)
+
+archivo = 'C:/Users/Usuario/Documents/ejemplo.txt'
+tamaño = os.path.getsize(archivo)
+
+print(f"El tamaño del archivo es: {tamaño} bytes")
+
+carpeta = 'C:/Users/Usuario/Documentos'
+contenido = os.listdir(carpeta)
+
+print("Contenido de la carpeta:")
+for elemento in contenido:
+    print(elemento)
+
+ruta = 'C:/Users/Usuario/Documentos/ejemplo.txt'
+
+# Verificar si la ruta existe
+if os.path.exists(ruta):
+    print("La ruta existe.")
+else:
+    print("La ruta no existe.")
+
+# Verificar si el archivo existe
+if os.path.isfile(ruta):
+    print("Es un archivo.")
+else:
+    print("No es un archivo.")
+
+# Verificar si la carpeta existe
+if os.path.isdir(ruta):
+    print("Es una carpeta.")
+else:
+    print("No es una carpeta.")
+
